5dc48bc0697795f0e9b36e013dd8f653ce4f7dd8
firewallfail <firewall.fail@hotmail.com>
2022-04-26 14:33:04 -0400

    Update counter to red when in negative numbers

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -1,8 +1,15 @@
+//Update the character counter changing red when numbers are negative
 $(document).ready(function() {
-
   const maxTweetLength = 140;
   $('#tweet-text').on('input', function() {
     let remainingCharacters = maxTweetLength - $(this).val().length;
-    $(this).next('.tweet-footer').children('.counter').text(remainingCharacters);
+    let output = $(this).next().children('output')
+    if (remainingCharacters < 0) {
+      output.addClass('counter-red');
+    }
+    if (remainingCharacters >= 0) {
+      output.removeClass('counter-red');
+    }
+    output.text(remainingCharacters);
   })
 });
